b82ba84bfa6c7099f178843e8ac5808984b64711
GioMurillo <giovanni.p.murillo@gmail.com>
2022-12-07 20:37:42 -0800

    Create game() which plays 5 rounds
    Need to change return valuesin playRound
    so I can keep count of the wins
    and report a winner

--- main.js
+++ main.js
@@ -12,25 +12,38 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerSelectionCaps = playerSelection.toUpperCase();
-    let computerSelectionCaps = computerSelection.toUpperCase();
-    if(playerSelectionCaps === computerSelectionCaps) {
-        return 'Tie! Keep it going!';
-    } else if(playerSelectionCaps === 'ROCK' && computerSelectionCaps === 'SCISSORS') {
-        return 'You win! Rock beats Scissors';
-    } else if(playerSelectionCaps === 'PAPER' && computerSelectionCaps === 'ROCK') {
-        return 'You win! Paper beats Rock';
-    } else if(playerSelectionCaps === 'SCISSORS' && computerSelectionCaps == 'PAPER') {
-        return 'You win! Scissors beats paper';
-    } else if(playerSelectionCaps === 'SCISSORS' && computerSelectionCaps == 'ROCK') {
-        return 'You lose! Rock beats Scissors';
-    } else if(playerSelectionCaps === 'PAPER' && computerSelectionCaps === 'SCISSORS') {
-        return 'You lose! Scissors beats paper';
-    } else if(playerSelectionCaps === 'ROCK' && computerSelectionCaps === 'PAPER') {
-        return 'You lose! Paper beats Rock';
-    }
+  let playerSelectionCaps = playerSelection.toUpperCase();
+  let computerSelectionCaps = computerSelection.toUpperCase();
+  if(playerSelectionCaps === computerSelectionCaps) {
+      return 'Tie! Keep it going!';
+  } else if(playerSelectionCaps === 'ROCK' && computerSelectionCaps === 'SCISSORS') {
+      return 'You win! Rock beats Scissors';
+  } else if(playerSelectionCaps === 'PAPER' && computerSelectionCaps === 'ROCK') {
+      return 'You win! Paper beats Rock';
+  } else if(playerSelectionCaps === 'SCISSORS' && computerSelectionCaps == 'PAPER') {
+      return 'You win! Scissors beats paper';
+  } else if(playerSelectionCaps === 'SCISSORS' && computerSelectionCaps == 'ROCK') {
+      return 'You lose! Rock beats Scissors';
+  } else if(playerSelectionCaps === 'PAPER' && computerSelectionCaps === 'SCISSORS') {
+      return 'You lose! Scissors beats paper';
+  } else if(playerSelectionCaps === 'ROCK' && computerSelectionCaps === 'PAPER') {
+      return 'You lose! Paper beats Rock';
+  }
 }
 
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  let playerSelection = '';
+  let computerSelection = '';
+  let winCount = 0;
+  for (let i = 0; i <5; i++) {
+    playerSelection = prompt('Enter rock, paper, or scissors', 'rock');
+    computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+
+
+
+// const playerSelection = 'rock';
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
